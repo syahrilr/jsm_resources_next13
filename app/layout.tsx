71986148921578c12a5d70d286bfdcd1702a0eb4
syahrilr <syahrilramadhan754@gmail.com>
2023-09-24 10:05:54 +0800

    fix skeleton color, improve metadata, change favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,18 @@ import './globals.css'
 
 
 export const metadata: Metadata = {
-  title: 'JS Mastery',
-  description: 'JS Mastery Resources',
-}
+  title: "JS Mastery",
+  description: "JS Mastery Resources",
+  other: {
+    "theme-color": "#0d1117",
+    "color-scheme": "dark only",
+    "twitter:image": "https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg",
+    "twitter:card": "summary_large_image",
+    "og:url": "jsmastery.pro",
+    "og:image": "https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg",
+    "og:type": 'website'
+  },
+};
 
 export default function RootLayout({
   children,
